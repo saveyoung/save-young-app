a48a8458af45f2d18f0ea1ff712a92db6ddca1fc
Kimberly Small <klsmall@coastal.edu>
2024-03-04 22:18:00 -0500

    Updates to resources page

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,6 +1,7 @@
 import Navigation from "../navigation";
 import Footer from "../footer";
 import ResourceRecord from "./resourceRecord";
+import Link from "next/link";
 
 export default function resources() {
     return (
@@ -10,22 +11,37 @@ export default function resources() {
                 <div className="quickContact">
                     <h1 className="sectionHeader">Keeping contacts handy!</h1>
                     <p className="sectionText">Important Hotline numbers to call for quick assistance can be life saving. Here is the information about Local authorities, National authorities and NGO’s near you!</p>
+                    <Link href="https://www.google.com/search?q=police+station+near+me" className="contactLink">Local Authorities</Link>
+                    <Link href="https://humantraffickinghotline.org/en/contact" className="contactLink">National Hotlines</Link>
+                    <Link href="https://www.google.com/search?q=ngo+near+me" className="contactLink">NGO's near you</Link>
                 </div>
                 <div className="resourceSection">
                     <h1 className="sectionHeader">Know more, protect more</h1>
                     <div className="resourceContainer">
-                        <ResourceRecord
-                            img=''
-                            description='Understanding digital risks in the era of social media'
-                        />
-                        <ResourceRecord
-                            img=''
-                            description='Kids influencers- the good and the bad side of kids on social media'
-                        />
-                        <ResourceRecord
-                            img=''
-                            description='2023 reports on Child safety in schools and public places'
-                        />
+                        <Link href="https://www.mayoclinichealthsystem.org/hometown-health/speaking-of-health/social-media-risks-tips-to-keep-your-children-safe">
+                            <ResourceRecord
+                                img='/images/socialMediaRisks.jpeg'
+                                imgAlt="Picture of teens on their phones."
+                                description="Social media risks: Safeguarding children's online experience"
+                                source="Mayo Clinic Health System"
+                            />
+                        </Link>
+                        <Link href="https://www.pbs.org/newshour/show/the-dangers-of-parents-sharing-their-childrens-lives-on-social-media">
+                            <ResourceRecord
+                                img='/images/kidsAsInfluencers.png'
+                                imgAlt="Picture of teens posing for a photo."
+                                description="The dangers of parents sharing their children’s lives on social media"
+                                source="PBS"
+                            />
+                        </Link>
+                        <Link href="https://www.ed.gov/human-trafficking">
+                            <ResourceRecord
+                                img='/images/childrenAtSchool.png'
+                                imgAlt="Picture of teens on their phones."
+                                description="Combatting Human Trafficking in America's Schools"
+                                source='U.S. Department of Education'
+                            />
+                        </Link>
                     </div>
                 </div>
             </div>
--- src/app/resources/resourceRecord.tsx
+++ src/app/resources/resourceRecord.tsx
@@ -1,17 +1,27 @@
+import Image from "next/image";
+
 type ResourceRecordProps = {
     img: string;
+    imgAlt: string;
     description: string;
+    source: string;
   };
 
-export default function resourceRecord({img, description}: ResourceRecordProps) {
+export default function resourceRecord({img, imgAlt, description, source}: ResourceRecordProps) {
     return (
         <div>
             <div className="resourceLink">
-                <div className="resourceImg">
-                    {img}
-                </div>
+                <Image src={img}
+                    width={104}
+                    height={78}
+                    alt={imgAlt}
+                    className="resourceImg"
+                />
                 <div className="resourceDescription">
                     <p>{description}</p>
+                    <div className="resourceSource">
+                        <p>Source: {source}</p>
+                    </div>
                 </div>
             </div>
             <hr className="resourceDivider"/>
